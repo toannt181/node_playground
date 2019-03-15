b094f11da8ff36d190c30555da3c8143a3b6e3dd
toannt181 <nguyen.trong.toan@framgia.com>
2019-03-15 21:07:05 +0700

    commit 1

---
+++ main.js
@@ -0,0 +1,15 @@
+import { API_VERSION } from './constants'
+
+window.axios = require('axios')
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
+window.axios.defaults.baseURL = API_VERSION
+
+const token = document.head.querySelector('meta[name="csrf-token"]')
+
+if (token) {
+  window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content
+} else {
+  console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token')
+}
+
